736df2bb1bcf7554e5c2a72ce25efe08d852dd49
mahfuz khan <mahfuzpust@gmail.com>
2023-02-27 23:54:15 +0600

    Add project files.

---
+++ ASPdotNetDesign/Controllers/EmployeController.cs
@@ -0,0 +1,19 @@
+﻿using ASPdotNetDesign.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPdotNetDesign.Controllers
+{
+    public class EmployeController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public EmployeController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = context.Employees.ToList();
+            return View(result);
+        }
+    }
+}
---
+++ ASPdotNetDesign/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ASPdotNetDesign.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+
+namespace ASPdotNetDesign.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
