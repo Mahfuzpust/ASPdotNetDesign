845fa7145ae97c3a3e6122c2982e6e2201aa45dc
mahfuz khan <mahfuzpust@gmail.com>
2023-03-03 19:26:01 +0600

    Employee-Student

--- ASPdotNetDesign/Controllers/EmployeController.cs
+++ ASPdotNetDesign/Controllers/EmployeController.cs
@@ -11,18 +11,19 @@ namespace ASPdotNetDesign.Controllers
         {
             this.context = context;
         }
+        // --Employee--
         public IActionResult Index()
         {
             var result = context.Employees.ToList();
             return View(result);
         }
-
+        // --Student--
         public IActionResult Student()
         {
             var studentResult = context.StudentInfoes.ToList();
             return View(studentResult);
         }
-
+        // --Employee Create--
         [HttpGet]
         public IActionResult Create()
         {
@@ -43,6 +44,37 @@ namespace ASPdotNetDesign.Controllers
                 context.Employees.Add(emp);
                 context.SaveChanges();
                 return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Empty field can not submit";
+                return View(model);
+            }
+        }
+
+        // --Student Create--
+        [HttpGet]
+        public IActionResult CreateStu()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateStu(StudentInfo model)
+        {
+            if (ModelState.IsValid)
+            {
+                var emp = new StudentInfo()
+                {
+                    Reg = model.Reg,
+                    Name = model.Name,
+                    Email = model.Email,
+                    Phone = model.Phone,
+                    Dept = model.Dept,
+                    varsity = model.varsity
+                };
+                context.StudentInfoes.Add(emp);
+                context.SaveChanges();
+                return RedirectToAction("Student");
             }
             else
             {
